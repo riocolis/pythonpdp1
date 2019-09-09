1a0e8f271e3a9931bec29ced46dd6faba1eae437
riocolis <riocolis@gmail.com>
2019-09-09 22:08:48 +0700

    Pythonpdp1

---
+++ case2.py
@@ -0,0 +1,21 @@
+a = 1
+b = 4.5
+c = 'kata'
+
+d = ['usia',30]
+e = ('usia',30)
+f = {'usia' : 30, 'pekerjaan' : 'mahasiswa'}
+
+print('latihan 1\n')
+print(type(a))
+print("\tNilai bilangan bulat a adalah \t: ",a,"\n")
+print(type(b))
+print("\tNilai bilangan decimal adalah \t: %f"%(b),"\n")
+print(type(c))
+print("\tKata dari c adalah \t\t: {}".format(c),"\n")
+print(type(d))
+print("\tDaftar dari d adalah \t\t: ",d,"\n")
+print(type(e))
+print("\tDaftar tuple dari e adalah \t: ",e,"\n")
+print(type(f))
+print("\tKomponen directory dari f adalah: ",f,"\n")
---
+++ case3.py
@@ -0,0 +1,6 @@
+print("------ Sengketa Lahan ----------\n")
+panjang = float(input("Panjang \t\t\t: "))
+lebar = float(input("Lebar \t\t\t\t: "))
+luas = panjang * lebar
+print("Luas tanah \t\t\t:",luas)
+print("Luas tanah di bagi 2 adalah \t:",(luas/2))
